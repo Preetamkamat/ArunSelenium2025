d2957d4bf5d1c60e9ffd393994000d8f7da46e39
Preetamkamat1702@gmail.com <31009397+Preetamkamat@users.noreply.github.com>
2025-09-26 19:33:32 +0530

    Selenium get started

---
+++ src/test/java/com/selenium/basicconcepts/Demo.java
@@ -0,0 +1,22 @@
+package com.selenium.basicconcepts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        //WebDriver driver = new EdgeDriver();
+        WebDriver driver = new InternetExplorerDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://omayo.blogspot.com/");
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, "omayo (QAFox.com)", "Title is not matching");
+        driver.quit();
+    }
+}
